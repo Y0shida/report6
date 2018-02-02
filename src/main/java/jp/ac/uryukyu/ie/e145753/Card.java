ec541fc6a5cb28b084358e8d9f9ab0a31e78b62c
Y0shida <e145753@ie.u-ryukyu.ac.jp>
2018-02-02 11:02:44 +0900

    Cardクラス追加

--- src/main/java/jp/ac/uryukyu/ie/e145753/Card.java
+++ src/main/java/jp/ac/uryukyu/ie/e145753/Card.java
@@ -1,4 +1,36 @@
 package jp.ac.uryukyu.ie.e145753;
 
 public class Card {
+
+    public final static int HEART = 0;
+    public final static int DIAMOND = 1;
+    public final static int SPADE = 2;
+    public final static int CLUB = 3;
+
+    public final static int NSUITS = 4;
+
+    public final static int NCARDS_PER_SUIT = 13;
+
+    private int suit;
+    private int number;
+
+    public Card(int suit, int number){
+        this.number = number;
+        this.suit = suit;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+
+    public String toString() {
+
+        String[] name = {"H", "D", "S", "C"};
+        return name[suit] + number;
+    }
 }
